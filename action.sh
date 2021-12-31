70b72ff4ce00d18efb624f53e1a4e1562ab96ee5
cir.no <deviser@tuta.io>
2021-12-31 13:54:07 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid && ls
-pip3 install -U -r re*/st*/optional-requirements.txt
-pip3 install -U -r requirements.txt
+pip3 install -U -r req*
+pip3 install -U -r res*/sta*/opt*
 python3 -m pyUltroid
